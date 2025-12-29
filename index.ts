1e0f957b3d8f37cb1ad31b911b65114825112099
Aminul Islam <5006546+AminulBD@users.noreply.github.com>
2025-12-29 15:53:52 +0600

    profile

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+  return new Response(JSON.stringify({
+    "name": "Aminul Islam",
+    "facebook": "https://www.facebook.com/aminul.run",
+    "linkedin" :"https://www.linkedin.com/in/aminulbd/"
+  }, null, 2), {
+    headers: { "content-type": "application/json" },
   });
 });
