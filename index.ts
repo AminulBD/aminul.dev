b086d86e4a73084708d6215ac95893319b3256fb
Aminul Islam <5006546+AminulBD@users.noreply.github.com>
2026-09-25 11:20:23 +0600

    fix: WebAPI only

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-Deno.serve(() => {
-  return new Response(JSON.stringify({
-    "name": "Aminul Islam",
-    "facebook": "https://www.facebook.com/aminul.run",
-    "linkedin" :"https://www.linkedin.com/in/aminulbd/"
-  }, null, 2), {
-    headers: { "content-type": "application/json" },
-  });
-});
+export default {
+  async fetch(): Promise<Response> {
+    return new Response(
+      JSON.stringify({
+          name: "Aminul Islam",
+          facebook: "https://www.facebook.com/aminul.run",
+          linkedin: "https://www.linkedin.com/in/aminulbd/",
+        }, null, 2),
+      { headers: { "content-type": "application/json" } },
+    );
+  },
+};
